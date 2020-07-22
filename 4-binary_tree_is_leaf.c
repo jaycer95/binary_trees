88f5437b6109baa3e200a7822545a50096dbb898
jaycer95 <jacer.dabbabi95@gmail.com>
2020-07-22 15:09:32 +0100

    4.2

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,11 +6,7 @@
  **/
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-if (!node)
-return (0);
-if (!node->right && !node->right)
+if (node && !node->right && !node->left)
 return (1);
-binary_tree_is_leaf(node->left);
-binary_tree_is_leaf(node->right);
 return (0);
 }
