4ae57bafb8554ba59cc6f1d02edff665f9f15d1e
nourouhichi <1417@holbertonschool.com>
2020-07-22 16:24:43 +0100

    new add

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	if (binary_tree_sibling((binary_tree_t *)first) == second)
+		return (first->parent);
+	if (binary_tree_uncle((binary_tree_t *)first) == second)
+	       return (second->parent);
+	if (binary_tree_uncle((binary_tree_t *)second) == first)
+		return (first->parent);
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+	if (second->parent == first)
+		return ((binary_tree_t *)first);
+	return (NULL);
+}
