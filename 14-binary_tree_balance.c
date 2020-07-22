a282346094ed350861a0c2d37fc68492207a398a
jaycer95 <jacer.dabbabi95@gmail.com>
2020-07-22 15:36:14 +0100

    14.2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t n, m;
-if (tree == NULL || (!tree->left && !tree->right))
+if (tree == NULL /*|| (!tree->left && !tree->right)*/)
 return  (0);
 n = binary_tree_height(tree->left);
 m = binary_tree_height(tree->right);
@@ -23,14 +23,13 @@ return (m + 1);
  **/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int l, r;
+int diff;
 if (tree == NULL)
 return (0);
 /*if (tree->left && !tree->right)
 return (1);
 if (!tree->left && tree->right)
 return (-1);*/
-l = binary_tree_height(tree->left);
-r = binary_tree_height(tree->right);
-return (l - r);
+diff = binary_tree_height(tree->left) - binary_tree_height(tree->right);
+return (diff);
 }
