313d6f8a65a11a378dec3c33b5a54f4eec5c0140
nourouhichi <1417@holbertonschool.com>
2020-07-22 14:59:02 +0100

    new fix

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
  *binary_tree_is_root - checks if the node is a root
- * @node: the node to be checked
- * Return: int
- * */
+ *@node: the node to be checked
+ *Return: int
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node && !node->parent)
-		return 1;
-	else 
-		return 0;
+		return (1);
+	else
+		return (0);
 }
