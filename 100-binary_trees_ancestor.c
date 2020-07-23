ced75084f67734449629779605436fd2e7d467cc
nourouhichi <1417@holbertonschool.com>
2020-07-23 13:49:22 +0100

    fix code problem

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,33 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_sibling - find node sibling
- * @node: node to find its sibling
- * Return: sibling node or NULL
- **/
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
-{
-if (node == NULL || node->parent == NULL)
-return (NULL);
-if (node == (node->parent)->right && (node->parent)->left)
-return ((node->parent)->left);
-if (node == (node->parent)->left && (node->parent)->right)
-return ((node->parent)->right);
-return (NULL);
-}
-/**
- * binary_tree_uncle - find node uncle
- * @node: node to find uncle
- * Return: uncle node or NULL
- **/
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
-{
-if (node == NULL || node->parent == NULL)
-return (NULL);
-if (binary_tree_sibling(node->parent))
-return (binary_tree_sibling(node->parent));
-return (NULL);
-}
-/**
  * binary_trees_ancestor - finds the lowest common ancestor
  * @first: the first node
  * @second: the second node
@@ -36,19 +8,23 @@ return (NULL);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	const binary_tree_t *tmp1, *tmp2;
+
 	if (!first || !second)
 		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
-	if (binary_tree_sibling((binary_tree_t *)first) == second)
-		return (first->parent);
-	if (binary_tree_uncle((binary_tree_t *)first) == second)
-		return (second->parent);
-	if (binary_tree_uncle((binary_tree_t *)second) == first)
-		return (first->parent);
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
-	if (second->parent == first)
-		return ((binary_tree_t *)first);
+	tmp1 = first;
+	while (tmp1)
+	{
+		tmp2 = second;
+		while (tmp2)
+		{
+			if (tmp2 == tmp1)
+				return ((binary_tree_t *)tmp1);
+			tmp2 = tmp2->parent;
+		}
+		tmp1 = tmp1->parent;
+	}
 	return (NULL);
 }
